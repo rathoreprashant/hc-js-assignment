8309c087da6bd35bffb539a88f7102ea88eb0d6d
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-07 18:59:42 +0530

    Js-module1_task4 is completed

---
+++ Js-module1/JSModule-1_task-4.js
@@ -0,0 +1,23 @@
+// a. every element being twice of elements in the current list.
+let twiceElement = function(arr) {
+    let twicearr = [];
+    for (const x of arr) {
+        const element = 2 * x;
+        twicearr.push(element);
+    }
+    return twicearr;
+}
+
+//b. every element being half of the elements in the current list.
+let halfElement = function(arr) {
+    let halfarr = [];
+    for (const x of arr) {
+        const element = x / 2;
+        halfarr.push(element);
+    }
+    return halfarr;
+}
+
+var arr = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+console.log("twice of elemnents ", twiceElement(arr));
+console.log("half of elemnents ", halfElement(arr));
