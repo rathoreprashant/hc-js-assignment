62445fcddfa70c8b432ffd0ba3e0e78cdd23c6c9
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-12 19:37:47 +0530

    Js-module-2_Task4 twice & half elements list

---
+++ Js-module2/Js-Module-2_Task4.js
@@ -0,0 +1,16 @@
+// 4. Provided the following  array:
+//    [0, 2, 4, 6, 8, 10, 12, 14, 16]
+//    Generate and print another list such that: 
+// a. every element being twice of elements in the current list.
+// b. every element being half of the elements in the current list.
+
+let array = [0, 2, 4, 6, 8, 10, 12, 14, 16];
+
+// TWICE of array's elements
+const twice = array.map(x => x * 2);
+
+// HALF of array's elements
+const half = array.map(x => x / 2);
+
+console.log(`twiced elements = [${twice}]`);
+console.log(`half elements = [${half}]`);
