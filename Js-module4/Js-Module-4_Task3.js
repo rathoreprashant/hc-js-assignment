7267941f5bd9b923aff67f1545e7d0b03030adce
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-21 12:33:51 +0530

    Js-module4_task3 sum using recursion

---
+++ Js-module4/Js-Module-4_Task3.js
@@ -0,0 +1,17 @@
+// 3. Implement code for sum using recursion
+const array = [10, 20, 30, 40];
+
+function sum(arr) {
+
+    if (array.length == 0) return 0;
+    return add(arr, 0);
+
+    function add(arr, curr) {
+        if (curr == arr.length - 1) return arr[curr];
+        return arr[curr] + add(arr, curr + 1);
+    }
+};
+
+const sumarr = sum(array);
+
+console.log(sumarr);
