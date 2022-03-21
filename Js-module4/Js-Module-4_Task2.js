703f087091c11ebb4b5a22f714aefefbc54dac78
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-21 12:32:04 +0530

    Js-module4_task2 Implement Regular Expression

---
+++ Js-module4/Js-Module-4_Task2.js
@@ -0,0 +1,10 @@
+// 2. Implement regular expression for name with letters
+
+const str = "mayank Prashant vivek";
+const regexp = /p/i;
+
+const findat = str.search(regexp);
+const result = regexp.test(str);
+
+console.log(str[findat]);
+result ? console.log(`the name exists with the letter 'P' at  ${findat}th index`) : console.log("the name doesn't exist with the letter 'P'");
