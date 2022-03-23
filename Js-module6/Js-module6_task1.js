b695c2558c163e7eab101a608d83a60dff8e7246
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-23 16:22:54 +0530

    Js-module6 change bg-color of col2

---
+++ Js-module6/Js-module6_task1.js
@@ -0,0 +1,11 @@
+// Task-1.To change the background color of the second column of a table having “n ” rows on click by using querySelector. -->
+
+let col2 = document.querySelectorAll("td:nth-child(2)");
+
+for (let x in col2) {
+    col2[x].addEventListener("click", function() {
+        col2.forEach(e => {
+            e.style.backgroundColor = "cyan";
+        });
+    });
+}
