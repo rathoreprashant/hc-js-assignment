232ab210b56c6882e0bf72cc297f71ae7dc1d61e
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-25 15:09:56 +0530

    Js-module7_task2 change cell-color & revert on click

---
+++ Js-module7/Js-module7_task2.js
@@ -0,0 +1,6 @@
+// Change the color of the cell to red when the user clicks on a cell and revert when the user again clicks.
+let cell = document.querySelectorAll("tr>th,td");
+for (let i of cell)
+    i.addEventListener("click", () => {
+        i.style.backgroundColor == '' ? i.style.backgroundColor = 'cyan' : i.style.backgroundColor = '';
+    });
