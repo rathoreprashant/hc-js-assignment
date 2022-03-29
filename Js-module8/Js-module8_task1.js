c1ceff43bb545729aa608af337c24e558965a99a
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-03-29 10:59:34 +0530

    Js-module8_task1 dynamically change image while loading

---
+++ Js-module8/Js-module8_task1.js
@@ -0,0 +1,10 @@
+let logo = document.querySelector('#myimg');
+
+logo.src = "NOimage.jpg"; //NOimage.jpg is not exist (image1.jpg exist)
+
+logo.addEventListener('load', () => alert('NO error found, Logo & document has been loaded successfully!'));
+
+logo.addEventListener('error', () => {
+    logo.src = "download.png";
+    alert('HotWax Logo has been changed due to some error');
+});
