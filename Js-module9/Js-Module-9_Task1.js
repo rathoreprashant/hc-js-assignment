7272aeddfef105b09a293a750186cfc626f7863d
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-04-05 16:49:46 +0530

    Js_module-11 fix Js_module-9(task1,task2) with Best practices

--- Js-module9/Js-Module-9_Task1.js
+++ Js-module9/Js-Module-9_Task1.js
@@ -1,8 +1,9 @@
+"use strict";
 // 1.Implement a simple code to use a Promise
 
 new Promise((resolve, reject) => {
     const a = Math.floor(Math.random() * 100);
     const b = Math.floor(Math.random() * 100);
     const c = a + b;
-    c % 2 == 0 ? resolve(c) : reject(c);
-}).then((result) => console.log("even", result)).catch((result) => console.log("odd", result));
+    c % 2 === 0 ? resolve(c) : reject(c);
+}).then(result => console.log("even", result)).catch(result => console.log("odd", result));
--- Js-module9/Js-module9_task2.js
+++ Js-module9/Js-module9_task2.js
@@ -1,3 +1,4 @@
+"use strict";
 // 2. Implement code to handle multiple promises. You have to fetch information of 5 different products from API
 async function getusersdata() {
     for (let x = 1; x <= 5; x++) {
@@ -5,10 +6,22 @@ async function getusersdata() {
             .then(res => res.json())
             .then(element => {
                 console.log("fetch", element);
-                document.getElementById(`response${x}`).innerHTML = "Id: " + element.id + "<br>Product Name: " + element.title +
-                    "<br>Price: " + element.price + "<br>Category: " + element.category;
-                document.getElementById(`img${x}`).src = element.image;
-            });
+                (function() {
+                    let head = document.createElement('h2');
+                    head.innerHTML = "Id: " + element.id + "<br>Product Name: " + element.title +
+                        "<br>Price: " + element.price + "<br>Category: " + element.category;
+                    document.body.appendChild(head);
+
+                    let image = document.createElement('img');
+                    image.src = element.image;
+                    image.width = "400";
+                    image.height = '400';
+                    document.body.appendChild(image);
+                }());
+                // document.getElementById(`response${x}`).innerHTML = "Id: " + element.id + "<br>Product Name: " + element.title +
+                //     "<br>Price: " + element.price + "<br>Category: " + element.category;
+                // document.getElementById(`img${x}`).src = element.image;
+            }).catch(error => console.log(error));
     }
 }
 getusersdata();
