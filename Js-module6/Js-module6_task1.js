e76ac0269345f4875b9be1cafb3c51c2918f3df7
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-04-05 15:14:30 +0530

    Js_module-11 fix Js_module-6(task1) with Best practices

--- Js-module6/Js-module6_task1.js
+++ Js-module6/Js-module6_task1.js
@@ -1,11 +1,7 @@
+// "use strict";
 // Task-1.To change the background color of the second column of a table having “n ” rows on click by using querySelector. -->
 
 let col2 = document.querySelectorAll("td:nth-child(2)");
 
-for (let x in col2) {
-    col2[x].addEventListener("click", function() {
-        col2.forEach(e => {
-            e.style.backgroundColor = "cyan";
-        });
-    });
-}
+for (let x in col2)
+    col2[x].onclick = () => col2.forEach(e => e.style.backgroundColor = "cyan");
