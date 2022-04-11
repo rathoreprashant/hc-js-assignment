0722cf56bf5c828d86e213b23a23ce52549fea34
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-04-11 16:55:56 +0530

    Vue_module1_task1 Hello-world Vue-App

---
+++ vue-module1_task-1/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  
+  <div class="hello">
+    <h1>{{msg}}</h1>
+ <hr><br>
+      <label for="txt">Enter Your Name Here</label><br>
+      <textarea id="txt" cols="30" rows="10" placeholder="Enter Your Name Here..." v-model="userName"></textarea> 
+
+      <p id="u-name">Hello {{userName}} </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+    userName:"world"
+    }
+  }
+}
+</script>
+
+
+<style>
+body{
+  background-color: cornsilk;
+}
+.hello{
+  background-color: aliceblue;
+  width:max-content;
+  margin: auto;
+}
+#u-name{
+  background-color: burlywood;
+  font-size: 5em;
+}
+input [type="text"]{
+  width: 100px;
+  height: 500px;
+}
+</style>
---
+++ vue-module1_task-1/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to my hello world App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
