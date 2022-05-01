443c62d2ea52e6632f74f989771889595659d513
prashant singh rathore <prashantsinghrathore29@gmail.com>
2022-05-01 23:41:00 +0530

    Vue_module6_task4: Use Axios cache plugin to cache product API

--- ecommerce-clone_web-app/src/main.js
+++ ecommerce-clone_web-app/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import wrapper from 'axios-cache-plugin'
+
+let http = wrapper(axios, {
+    maxCacheSize: 15, // cached items amounts. if the number of cached items exceeds, the earliest cached item will be deleted. default number is 15.
+    ttl: 60000, // time to live. if you set this option the cached item will be auto deleted after ttl(ms).
+    excludeHeaders: true // should headers be ignored in cache key, helpful for ignoring tracking headers
+})
+export default http
+
 
 createApp(App).use(router).mount('#app')
--- ecommerce-clone_web-app/src/views/LoginView.vue
+++ ecommerce-clone_web-app/src/views/LoginView.vue
@@ -49,6 +49,30 @@
 
 <script>
 import axios from "axios";
+import { setupCache } from 'axios-cache-adapter'
+
+// Create `axios-cache-adapter` instance
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000
+})
+// Create `axios` instance passing the newly created `cache.adapter`
+const api = axios.create({
+  adapter: cache.adapter
+})
+// Send a GET request to some REST api
+api({
+  url: 'https://fakestoreapi.com/products',
+  method: 'get'
+}).then(async (response) => {
+  // Do something fantastic with response.data \o/
+  console.log('Request response:', response)
+  // Interacting with the store, see `localForage` API.
+  const length = await cache.store.length();
+  console.log('Cache store length:', length)
+})
+
+
+
 axios.interceptors.request.use((config) => {
   console.log(`${config.method} request sent to [ ${config.url}]`); 
   return config;
